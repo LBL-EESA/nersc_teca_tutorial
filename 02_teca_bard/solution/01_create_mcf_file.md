34c9ca966927d5388c00e8a68fd395eddee790cf
Travis A. O'Brien <TAOBrien@lbl.gov>
2023-10-02 08:03:35 -0700

    Refactored directory

--- 02_teca_bard/solution/01_create_mcf_file.md
+++
@@ -1,1 +0,0 @@
-In this step, manually create a MCF (multi cf, as in 'CF conventions') netCDF reader file; this tells TECA where to find all the various variables to use in the analysis.
--- 02_teca_bard/solution/03_remove_single_timestep_file.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# TECA BARD produces one file (for 2015) that has a single timestep;
-# that causes problems for the daily average operator, which is a bug we need to fix.  For now, this script removes that file.
-rm teca_bard_output/teca_bard.b.e21.BHISTsmbb.f09_g17.LE2-1251.020.cam.h2.2015-01-01-00Z.nc
